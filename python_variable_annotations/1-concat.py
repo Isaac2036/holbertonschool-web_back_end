8b4e1ae52b1ec7b288aa4f11b8946109957b4f63
Isaac2036 <isaacpablo0596@hotmail.com>
2024-02-17 10:42:06 -0500

    Update 1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-''' Description: defines a Python function called concat that 
-takes two string arguments, str1 and str2, and returns a new 
+''' Description: defines a Python function called concat that
+takes two string arguments, str1 and str2, and returns a new
 string that is the concatenation of str1 and str2.
     arguments: str1: str
                str2: str
