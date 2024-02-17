8faf41ac859719371722699947b2bbebda80a970
Isaac2036 <isaacpablo0596@hotmail.com>
2024-02-17 11:55:34 -0500

    Update 7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-''' Description: defines a function called to_kv that takes two arguments, 
-k of type str and v of type int or float, and returns a tuple containing 
+''' Description: defines a function called to_kv that takes two arguments,
+k of type str and v of type int or float, and returns a tuple containing
 the key-value pair (k, v^2).
     Arguments: k: str, v: Union[int, float]
 '''
