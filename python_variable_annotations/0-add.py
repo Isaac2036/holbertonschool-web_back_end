20e92bd0ba6ea28a7508d25d4944cadaf2183444
Isaac2036 <isaacpablo0596@hotmail.com>
2024-02-17 01:17:31 -0500

    Update 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-''' Description: This code defines a function called add that takes two 
-    arguments,a and b, which are both of type float. The function 
+''' Description: This code defines a function called add that takes two
+    arguments,a and b, which are both of type float. The function
     returns the sum of a and b.
     Arguments: a: float
                 b: float
