e8e658b9b7bd5165f5b10b22d55927d2bbbe546c
Isaac2036 <isaacpablo0596@hotmail.com>
2024-02-25 12:36:38 -0500

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Se genera un wait_random n veces con el max_delay especificado 
+    """Se genera un wait_random n veces con el max_delay especificado
     y devuelve la lista de todos los retardos (float)."""
     futures = [wait_random(max_delay) for _ in range(n)]
     futures = asyncio.as_completed(futures)
